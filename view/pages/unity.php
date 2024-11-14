c75b0cab38c8f3b719041cea863f740bec45827d
PauloDiney <paulo19martin@gmail.com>
2024-11-13 23:04:53 -0300

    Pagina/Unity

--- view/pages/unity.php
+++ view/pages/unity.php
@@ -17,22 +17,82 @@
     <?php
     include '../Components/MenuBarra.php';
     ?>
-    <section class="unity-base-meio" data-aos="fade-up" data-aos-duration="2000">
+    <section class="unity-base-meio">
         <div class="unity-base-container">
             <section class="unity-base" data-aos="fade-up" data-aos-duration="2000">
                 <img src="../img/unity.png" alt="UnityVerse Logo">
             </section>
         </div>
     </section>
-    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="2000">
+    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="1000" >
         <div class="text-content">
             <h2>Informações Adicionais</h2>
-            <p>Liberte sua criatividade
-                O motor líder da indústria da Unity fornece ferramentas para criar e operar incríveis games e outras experiências interativas em tempo real e publicá-las em uma ampla gama de dispositivos.
-                Unity 6, a versão mais recente, vem carregada com renderização mais rápida, opções de iluminação poderosas, fluxos de trabalho multiplayer de ponta a ponta, capacidades de IA expandidas e suporte mais profundo para tempos de execução na web móvel.</p>
+            <p>O mecanismo líder do setor Unity fornece ferramentas para criar e operar jogos incríveis e outras experiências interativas em tempo real e publicá-los em uma ampla variedade de dispositivos.
+
+                Unity 6, a versão mais recente, vem com renderização mais rápida, opções de iluminação poderosas, fluxos de trabalho multijogador completos, recursos expandidos de IA e suporte mais profundo para tempos de execução da Web móvel.</p>
         </div>
         <video src="../Movies/2.mp4" loop autoplay muted></video>
     </section>
+    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="text-content">
+            <h2>Crie uma vez, envie para qualquer lugar</h2>
+            <p>Esteja onde seus jogadores estão com suporte para novas plataformas e tecnologias inovadoras. Crie seu conteúdo uma vez e implante em todas as principais plataformas de AR, VR, dispositivos móveis, desktop e console</p>
+        </div>
+        <video src="../img/unity1.png" loop autoplay muted></video>
+    </section>
+    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="text-content">
+            <h2>Crie jogos para celular</h2>
+            <p>Unity aumenta o sucesso dos jogos para dispositivos móveis com ferramentas rápidas de prototipagem, otimização de desempenho e aumento de receita, melhorando o envolvimento dos jogadores em todo o ciclo de vida do jogo.</p>
+        </div>
+        <video src="../img/unity2.png" loop autoplay muted></video>
+    </section>
+    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="text-content">
+            <h2>Liberte a criatividade com ferramentas poderosas</h2>
+            <p>Crie experiências lindas e envolventes que envolvem desde os primeiros pixels. Com o Unity, as equipes criativas podem interagir rapidamente no mesmo ambiente. Desde construção de mundo, animação e cinemática até renderização, o Unity Editor funciona perfeitamente com ferramentas como Maya ou Blender para manter artistas e designers no fluxo.</p>
+        </div>
+        <video src="../img/unity3.png" loop autoplay muted></video>
+    </section>
+    <section class="unity-info-container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="text-content">
+            <h2>Uma plataforma rápida e flexível para programadores</h2>
+            <p>Coloque ótimos jogos nas mãos de seus jogadores com mais rapidez.O Unity permite prototipagem rápida e pipelines de ativos escalonáveis ​​com um editor personalizável. Implante código C# de alto desempenho em mais de 20 plataformas.</p>
+        </div>
+        <video src="../img/unity4.png" loop autoplay muted></video>
+    </section>
+    <section class="unity-image-section" data-aos="fade-up" data-aos-duration="1000">
+        <div class="unity-image-wrapper">
+            <img src="../img/unityPaper.png" alt="">
+            <div class="unity-container-wrapper">
+                <div class="unity-container">
+                    <a href="https://youtu.be/hMrPCkCqb4U?si=qgfpMjQ--L1K-27g">
+                        <img src="../img/unity5.png" alt="">
+                        <h1>Inicio na Unity</h1>
+                        <p>Aqui você ira aprender o inicio de tudo deste os componentes e </p>
+                        <p> campos e até programção do personagem </p>
+                    </a>
+                </div>
+                <div class="unity-container">
+                    <a href="https://youtu.be/MaevhxkIVqM?si=Z0reU2rt8crIpDfC">
+                        <img src="../img/unity6.png" alt="">
+                        <h1>Criar Mapa na Unity</h1>
+                        <p>Crie mapas em incriveis</p>
+                        <p>  </p>
+                    </a>
+                </div>
+                <div class="unity-container">
+                    <a href="https://youtu.be/WYGN2V1yHDo?si=ceY9qyAXwC14VcEf">
+                        <img src="../img/unity7.png" alt="">
+                        <h1>Programação na Unity</h1>
+                        <p>A programação vai ser uma grande aliada ao seu jogo</p>
+                        <p> aprenda, estude se esforce  !!</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         AOS.init();
